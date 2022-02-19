c7ae5ca2ebf89b38c4c766ca46c4adf4af28e864
dreinasant <ao.andreina@gmail.com>
2022-02-19 00:11:32 -0300

    desafio-react-02-calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calc extends Component {
+  state = {
+    n1: "",
+    n2: "",
+    result: "",
+    sinal: ""
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      n1: Number(event.target.value)
+    });
+  };
+
+  handleChangeTwo = (event) => {
+    this.setState({
+      n2: Number(event.target.value)
+    });
+  };
+
+  c = () => {
+    const { n1, n2, result } = this.state;
+    this.setState({
+      n1: "",
+      n2: "",
+      result: "",
+      sinal: ""
+    });
+  };
+
+  mult = () => {
+    const { n1, n2 } = this.state;
+    if (n1 && n2 !== "") {
+      this.setState({
+        result: n1 * n2,
+        sinal: "*"
+      });
+    }
+  };
+
+  dividir = () => {
+    const { n1, n2 } = this.state;
+    if (n1 && n2 !== "") {
+      this.setState({
+        result: n1 / n2,
+        sinal: "/"
+      });
+    }
+  };
+
+  subtrair = () => {
+    const { n1, n2 } = this.state;
+    if (n1 && n2 !== "") {
+      this.setState({
+        result: n1 - n2,
+        sinal: "-"
+      });
+    }
+  };
+
+  somar = () => {
+    const { n1, n2 } = this.state;
+    if (n1 && n2 !== "") {
+      this.setState({
+        result: n1 + n2,
+        sinal: "+"
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="box">
+        <h1>Calculadora básica</h1>
+        <input onChange={this.handleChange} type="text" value={this.state.n1} />
+        <h3>{this.state.sinal}</h3>
+        <input
+          onChange={this.handleChangeTwo}
+          type="text"
+          value={this.state.n2}
+        />
+        <h2>{this.state.result}</h2>
+        <div className="contain">
+          <button onClick={this.c}>C</button>
+          <button onClick={this.mult}>&times;</button>
+          <button onClick={this.dividir}>&divide;</button>
+          <button onClick={this.subtrair}>-</button>
+          <button onClick={this.somar}>+</button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Calc;
